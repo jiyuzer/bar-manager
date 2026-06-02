149b7bc0a32d6c0262be808d2a793fb58ef2bf7e
jiyuzer <arthurbervas1@gmail.com>
2026-06-02 15:11:19 +0200

    Add table names

---
+++ src/constants/tables.ts
@@ -0,0 +1,7 @@
+export const TABLE_NAMES = [
+  'Table 1', 'Table 2', 'Table 3', 'Table 4', 'Table 5',
+  'Table 6', 'Table 7', 'Table 8', 'Table 9', 'Table 10',
+  'Table 11', 'Table 12', 'Table 13', 'Table 14', 'Table 15',
+  'Table 16', 'Table 17', 'Table 18', 'Table 19', 'Table 20',
+  'Table 21', 'Table 22',
+];
